491d891541b09d589b03dd57ead0163372fc8790
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-17 10:19:26 -0300

    feat: create countries component

---
+++ src/components/Countries.js
@@ -0,0 +1,71 @@
+import usePagination from '../hooks/usePagination';
+
+const Countries = ({ data, itemsPerPage, startFrom }) => {
+  const { slicedData, pagination, prevPage, nextPage, changePage } =
+    usePagination({ itemsPerPage, data, startFrom });
+  return (
+    <>
+      <table className="table is-fullwidth is-striped">
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Capital</th>
+            <th>Code</th>
+            <th>Currency</th>
+            <th>Phone Code</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {slicedData.map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.capital}</td>
+              <td>{item.iso2}</td>
+              <td>{item.currency}</td>
+              <td>{item.phone_code}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <nav className="pagination">
+        <a href="/#" className="pagination-previous" onClick={prevPage}>
+          Previous
+        </a>
+        <a href="/#" className="pagination-next" onClick={nextPage}>
+          Next
+        </a>
+        <ul className="pagination-list">
+          {pagination.map(page => {
+            if (page.ellipsis) {
+              return (
+                <li key={page.id}>
+                  <span className="pagination-ellipsis">&hellip;</span>
+                </li>
+              );
+            }
+
+            return (
+              <li key={page.id}>
+                <a
+                  href="/#"
+                  className={
+                    page.current
+                      ? 'pagination-link is-current'
+                      : 'pagination-link'
+                  }
+                  onClick={event => changePage(page.id, event)}
+                >
+                  {page.id}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Countries;
