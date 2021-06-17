e907291057b53eacbbb353c0cb6999edfcfe66b6
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-17 10:25:01 -0300

    feat: update app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+
 import './App.css';
+import countries from './data/countries.json';
+import Header from './components/Header';
+import Countries from './components/Countries';
 
 const App = () => {
+  const [data] = useState(countries);
+
   return (
-    <div>
-      <p>React Pagination</p>
-    </div>
+    <>
+      <Header title="ReactJS pagination using custom hook" />
+      <div className="container px2">
+        <Countries data={data} itemsPerPage={10} />
+        <Countries data={data} itemsPerPage={5} startFrom={25} />
+      </div>
+    </>
   );
 };
 
