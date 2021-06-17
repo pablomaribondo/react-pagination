1d1621c6afa4a3e33afc4c84929225ce12654143
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-17 10:06:19 -0300

    feat: create header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = ({ title }) => {
+  return (
+    <div className="hero is-light has-text-centered">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">{title}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
